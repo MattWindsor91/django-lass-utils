e252bffb60b6a88d6d615f1a1a4c8502ecb6530d
Matt "Hayashi" Windsor <matt.windsor@ury.york.ac.uk>
2012-11-19 18:16:48 +0000

    forgot to amend some __init__

--- lass_utils/mixins/__init__.py
+++ lass_utils/mixins/__init__.py
@@ -1,6 +1,6 @@
 """
 The `lass-utils` module contains many mixins for adding common
-patterns, both pertinent to the metadata subsystem and not, to
+patterns, both pertinent to the lass_utils subsystem and not, to
 existing models.
 
 Many of these mixins are abstract models that add in new fields on
@@ -8,6 +8,6 @@ existing models; you may need to remove direct inheritance from
 `django.db.models.Model` to make these work.
 
 """
-from metadata.mixins.submittable import SubmittableMixin
-from metadata.mixins.date_range import DateRangeMixin
-from metadata.mixins.effective_range import EffectiveRangeMixin
+from lass_utils.mixins.submittable import SubmittableMixin
+from lass_utils.mixins.date_range import DateRangeMixin
+from lass_utils.mixins.effective_range import EffectiveRangeMixin
